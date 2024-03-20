37a4595fddbfe568e0b600094fe627a989bae3f7
ckiroshan <ckiroshan@gmail.com>
2024-03-20 12:08:46 +0530

    feat: Delete user from action button

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Link } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 function Home() {
   const [users, setUsers] = useState([]);
+
+  const { id } = useParams();
 
   useEffect(() => {
     loadUsers();
@@ -12,6 +14,11 @@ function Home() {
   const loadUsers = async () => {
     const results = await axios.get("http://localhost:8080/users");
     setUsers(results.data);
+  };
+
+  const deleteUser = async (id) => {
+    await axios.delete(`http://localhost:8080/users/${id}`);
+    loadUsers();
   };
 
   return (
@@ -35,9 +42,9 @@ function Home() {
                 <td>{user.username}</td>
                 <td>{user.email}</td>
                 <td>
-                  <button className="btn btn-primary mx-2">View</button>
-                  <Link to={`/edituser/${user.id}`} className="btn btn-outline-primary mx-2">Edit</Link>
-                  <button className="btn btn-danger mx-2">Delete</button>
+                  <button className="btn btn-danger mx-2" onClick={() => deleteUser(user.id)}>
+                    Delete
+                  </button>
                 </td>
               </tr>
             ))}

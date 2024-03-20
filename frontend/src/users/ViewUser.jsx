ca8a1ce1be241ee2472b26cd255b956191e01abb
ckiroshan <ckiroshan@gmail.com>
2024-03-20 19:31:10 +0530

    Impl: View user data from database

--- frontend/src/users/ViewUser.jsx
+++ frontend/src/users/ViewUser.jsx
@@ -1,22 +1,44 @@
-import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
 
 const ViewUser = () => {
+  const [user, setUser] = useState({
+    name: "",
+    username: "",
+    email: "",
+  });
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
+  const loadUser = async () => {
+    const result = await axios.get(`http://localhost:8080/users/${id}`);
+    setUser(result.data);
+  };
+
   return (
     <div className="container">
       <div className="row">
         <div className="col-md-6 offset-md-3 border rounded p-4 mt-2 shadow">
-          <h2 className="text-center m-4">User Details</h2>
+          <h2 className="text-center m-4">User Details of ID: {user.id}</h2>
           <div className="card">
             <div className="card-header p-4">
               <ul className="list-group list-group-flush p-2">
                 <li className="list-group-item">
-                  <b>Name:</b>
+                  <b>Name: </b>
+                  {user.name}
                 </li>
                 <li className="list-group-item">
-                  <b>Username:</b>
+                  <b>Username: </b>
+                  {user.username}
                 </li>
                 <li className="list-group-item">
-                  <b>Email:</b>
+                  <b>Email: </b>
+                  {user.email}
                 </li>
               </ul>
             </div>
